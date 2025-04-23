3505b6df5c5736b847e3c318b87bccaf6a0ae588
Varshu99 <72430323+Varshu99@users.noreply.github.com>
2025-04-23 06:06:14 +0000

    second commit

--- ngo-main/heart_charity/urls.py
+++ ngo-main/heart_charity/urls.py
@@ -3,11 +3,19 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-        path('',views.index, name="index" ),
+        path('',views.home, name="home" ),
         path('submit-valunteer',views.submit_valunteer, name="submit-valunteer" ),
         path('contact',views.contact, name="contact" ),
         path('donate/<int:id>',views.donate, name="donate" ),
         path('signup/', views.signup_view, name='signup'),
         path('signin', views.signin_view, name='signin'),
+        path(
+        "request_password_reset/",
+        views.request_password_reset,
+        name="request_password_reset",
+    ),
+        path("reset_password/<uname>/", views.reset_password, name="reset_password"),
+        path("logout", views.logout_view, name="logout"),  # Logout page
+
 
 ]
--- ngo-main/heart_charity/views.py
+++ ngo-main/heart_charity/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render, redirect
 from .models import Volunteer, Contact, Cause,Donate
+from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
-def index(request):
-    causes=Cause.objects.all()
-    return render(request,'index.html',{"causes":causes})
+# def base(request):
+#     causes=Cause.objects.all()
+#     return render(request,'base.html',{"causes":causes})
+def home(req):
+    return render(req,'home.html')
+    
+def logout_view(req):
+    logout(req)
+    return redirect("home")
 
 def submit_valunteer(request):
     if request.method =="POST":
@@ -32,6 +41,7 @@ def contact(request):
     else:
         return redirect('/')
 
+@login_required(login_url=reverse_lazy("login"))
 def donate(request,id):
     if request.method =="POST":
         name=request.POST['name']
@@ -83,4 +93,44 @@ def signin_view(request):
             return redirect('signin')
     return render(request, 'signin.html')
 
+
+def request_password_reset(req):
+    if req.method == "GET":
+        return render(req, "request_password_reset.html")
+    else:
+        uname = req.POST.get("uname")
+        context = {}
+        try:
+            userdata = User.objects.get(username=uname)
+            return redirect("reset_password", uname=userdata.username)
+
+        except User.DoesNotExist:
+            context["errmsg"] = "No account found with this username"
+            return render(req, "request_password_reset.html",context)
+
+def reset_password(req, uname):
+    userdata = User.objects.get(username=uname)
+    if req.method == "GET":
+        return render(req, "reset_password.html", {"user": userdata.username})
+    else:
+        upass = req.POST["upass"]
+        ucpass = req.POST["ucpass"]
+        context = {}
+        userdata = User.objects.get(username=uname)
+        try:
+            if upass == "" or ucpass == "":
+                context["errmsg"] = "Field can't be empty"
+                return render(req, "reset_password.html", context)
+            elif upass != ucpass:
+                context["errmsg"] = "Password and confirm password need to match"
+                return render(req, "reset_password.html", context)
+            else:
+                # validate_password(upass)
+                userdata.set_password(upass)
+                userdata.save()
+                return redirect("signin")
+
+        except ValidationError as e:
+            context["errmsg"] = str(e)
+            return render(req, "reset_password.html", context)
 
